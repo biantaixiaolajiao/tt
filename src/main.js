fbc1e7dd58a1257ceece61b9bfd73f87a0e2e072
biantaixiaolajiao <1334451138@qq.com>
2019-07-27 16:17:32 +0800

    initall

--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuex from 'vuex'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
+Vue.use(Vuex)
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
